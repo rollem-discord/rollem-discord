34de93200f3016b052b8ddda0cd7fd65359d1b5a
Lemtzas <lemtzas@gmail.com>
2018-07-28 22:43:02 -0700

    Fixing the secret replacement script

--- infra/scripts/add-secrets-to-templates.sh
+++ infra/scripts/add-secrets-to-templates.sh
@@ -7,10 +7,10 @@ ENV_SOURCE=infra/secrets/source.env
 
 source $ENV_SOURCE
 
-ALL_INFRA_TEMPLATES=`find infra | grep \.template\.`
+ALL_INFRA_TEMPLATES=`find infra | grep '\.template\.'`
 
 for file in $ALL_INFRA_TEMPLATES; do
-    target_file=$(sed s/\.template\././ <<< $file)
+    target_file=$(sed 's/\.template\././' <<< $file)
     echo $file to $target_file
     cat $file | envsubst > $target_file
 done
