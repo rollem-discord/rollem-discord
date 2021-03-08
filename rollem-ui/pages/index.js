cedb731aaac4b9e771f540d3c6264d404d867c41
David S <github@davidsharer.com>
2021-03-07 21:11:52 -0800

    Fix build and local run

--- rollem-ui/pages/index.js
+++ rollem-ui/pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Rollem</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
