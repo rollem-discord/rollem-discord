db70c4a5a7ca021e46e7419eed82f77f5d76e483
David S <github@davidsharer.com>
2021-03-07 22:09:20 -0800

    Typescript; more tutorial

---
+++ rollem-ui/pages/index.tsx
@@ -0,0 +1,54 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import { getSortedDocsData } from '../lib/get-docs-data';
+import styles from '../styles/Home.module.css';
+
+export default function Home({ allDocsData }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Rollem</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Link href="/docs">Docs</Link>
+      <Link href="/presskit">Presskit</Link>
+
+      {/* Add this <section> tag below the existing <section> tag */}
+      <section>
+        <h2>Blog</h2>
+        <ul>
+          {allDocsData.map(({ id, date, title }) => (
+            <li key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  )
+}
+
+export async function getStaticProps() {
+  const allDocsData = getSortedDocsData();
+  return {
+    props: {
+      allDocsData,
+    }
+  }
+}
