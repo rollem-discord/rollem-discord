49f6ccdc372834d6938864313024d6bb7ab153cd
David S <github@davidsharer.com>
2021-03-07 21:22:49 -0800

    titles

--- rollem-ui/pages/docs/index.js
+++ rollem-ui/pages/docs/index.js
@@ -1,10 +1,16 @@
 import Link from 'next/link';
+import Head from 'next/head';
 
 export default function DocsIndex() {
   return (
-    <ul>
-      <li><Link href="/">Home</Link></li>
-      <li><Link href="/docs/rolling">Rolling Docs</Link></li>
-    </ul>
+    <>
+      <Head>
+        <title>Rollem Docs Home</title>
+      </Head>
+      <ul>
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/docs/rolling">Rolling Docs</Link></li>
+      </ul>
+    </>
   )
 }
--- rollem-ui/pages/docs/rolling/index.js
+++ rollem-ui/pages/docs/rolling/index.js
@@ -1,10 +1,16 @@
 import Link from 'next/link';
+import Head from 'next/head';
 
 export default function DocsRollingIndex() {
   return (
+    <>
+    <Head>
+      <title>Rollem Rolling Docs</title>
+    </Head>
     <ul>
       <li><Link href="/">Home</Link></li>
       <li><Link href="/docs"> Docs</Link></li>
     </ul>
+    </>
   )
 }
