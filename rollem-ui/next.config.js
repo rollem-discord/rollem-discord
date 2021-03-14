b3c35b38fb1b77971481b2c546c6088e133fb4b7
David S <github@davidsharer.com>
2021-03-14 15:01:27 -0700

    Trying to get next APIs to work on netlify

---
+++ rollem-ui/next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // // Target must be serverless
+  // target: 'serverless',
+  // Target must be experimental-serverless-trace
+  // Your build time will be longer with this option
+  target: "experimental-serverless-trace",
+};
