0b16e9189ccf4dd1a512f5b46e893305547a26b0
David S <github@davidsharer.com>
2021-05-09 02:14:39 -0700

    cleanup

--- packages/language/config/webpack.config.js
+++ packages/language/config/webpack.config.js
@@ -1,73 +1,14 @@
-const path = require('path')
-const { IgnorePlugin } = require('webpack');
-const FilterWarningsPlugin = require('webpack-filter-warnings-plugin');
 const WebpackShellPluginNext = require('webpack-shell-plugin-next');
 
-module.exports = {
-  mode: 'production',
-  entry: './src/index.ts',
-  target: 'node',
-  node: {
-    global: true,
-    __filename: false,
-    __dirname: false,
-  },
-  plugins: [
-    new IgnorePlugin({ resourceRegExp: /^pg-native$/}),
-    new WebpackShellPluginNext({
-      onBuildStart: {
-        scripts: ['yarn run watch:lang:pegjs'],
-        blocking: false,
-        parallel: false,
-      },
-    }),
-    new WebpackShellPluginNext({
-      onBuildStart: {
-        scripts: ['yarn run build:lang:pegjs'],
-        blocking: true,
-        parallel: false,
-      },
-    }),
-    new FilterWarningsPlugin({
-      exclude: [/hdb-pool/, /@sap\/hana-client/, /mongodb/, /mssql/, /mysql/, /mysql2/, /oracledb/, /pg/, /pg-query-stream/, /react-native-sqlite-storage/, /redis/, /sqlite3/, /sql.js/, /typeorm-aurora-data-api-driver/]
-    }),
-    // new CopyPlugin({
-    //   patterns: [
-    //     { from: "**/*", to: "" },
-    //   ],
-    // }),
-  ],
-  output: {
-    path: path.resolve(__dirname, '../dist/umd'),
-    filename: 'index.js',
-    library: 'exampleTypescriptPackage',
-    libraryTarget: 'umd',
-    globalObject: 'this',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts(x*)?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'ts-loader',
-          options: {
-            configFile: 'config/tsconfig.umd.json',
-          },
-        },
-      },
-      {
-        test: /\.pegjs$/,
-        use: "pegjs-loader",
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js", ".pegjs"],
-    alias: {
-      "@language-v1": path.resolve(__dirname, "../src/rollem-language-1/"),
-      "@language-v1-beta": path.resolve(__dirname, "../src/rollem-language-1-beta/"),
-      "@language-v2": path.resolve(__dirname, "../src/rollem-language-2/"),
+const config = require('./webpack.base.config');
+config.plugins.push(
+  new WebpackShellPluginNext({
+    onBuildStart: {
+      scripts: ['yarn run build:lang:pegjs'],
+      blocking: false,
+      parallel: false,
     },
-  },
-}
+  }),
+);
+
+module.exports = config;
