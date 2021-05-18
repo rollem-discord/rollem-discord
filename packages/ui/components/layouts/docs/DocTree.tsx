e855212a686e1a53e8e26c41c5799444f3504c43
David S <github@davidsharer.com>
2021-05-17 22:46:14 -0700

    towards mobile layout

--- packages/ui/components/layouts/docs/DocTree.tsx
+++ packages/ui/components/layouts/docs/DocTree.tsx
@@ -1,27 +1,88 @@
+import { Collapse, List, ListItem, ListItemText, ListSubheader, Typography } from '@material-ui/core';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { ExpandLess, ExpandMore } from '@material-ui/icons';
+import { useState } from 'react';
 import { DocsData, DocsDataTree } from '../../../lib/markdown/docs/get-docs-data';
 import styles from './DocTree.module.scss';
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: '100%',
+      minHeight: '100%',
+      maxWidth: 360,
+      backgroundColor: theme.palette.background.paper,
+    },
+    nested: {
+      paddingLeft: theme.spacing(4),
+    },
+    listHeader: {
+      borderBottom: '1px solid',
+      borderColor: theme.palette.divider,
+    }
+  }),
+);
+
 export default function DocTree({ allDocsData }: { allDocsData: DocsDataTree[] }): JSX.Element {
+  const classes = useStyles();
+
   return (
     <section className={styles.root}>
-      <h3><b><u>Docs</u></b></h3>
-      <ul>
-        {allDocsData.map(node => makeTree(node))}
-      </ul>
+      <List
+        aria-labelledby="nested-list-subheader"
+        
+        subheader={
+          <ListSubheader component="div" id="nested-list-subheader">
+            <Typography variant="h5" align="center" className={classes.listHeader}>
+              Docs
+            </Typography>
+          </ListSubheader>
+        }
+        className={classes.root}
+        dense
+      >
+        {allDocsData.map((node) => makeTree(node))}
+      </List>
     </section>
   );
 }
 
-function makeTree(treeNode: DocsDataTree): JSX.Element {
+function makeTree(treeNode: DocsDataTree, nested: boolean = false): JSX.Element {
+  const [open, setOpen] = useState(true);
+  const classes = useStyles();
+
   let hasChildren = treeNode.children && treeNode.children.length > 0;
-  let children = hasChildren ? <ul>{treeNode.children.map(child => makeTree(child))}</ul> : <></>;
+  
+  let children = <></>;
+  let openToggle = <></>;
+  let handleClick = (event: React.MouseEvent) => { };
+  if (hasChildren) {
+    children = (
+      <Collapse in={open} timeout="auto" unmountOnExit>
+        <List disablePadding dense>
+          {treeNode.children.map(child => makeTree(child, true))}
+        </List>
+      </Collapse>
+    );
+
+    openToggle = open ? <ExpandLess /> : <ExpandMore />;
+    
+    handleClick = (event: React.MouseEvent) => {
+      setOpen(!open);
+      event.stopPropagation();
+    };
+  }
+
+  const path = '/docs/' + treeNode.item.route.join('/');
   return (
-    <li className={hasChildren ? styles.hasChildren : null} key={treeNode?.item?.id}>
-      {makeEntry(treeNode?.item)}
+    <>
+      <ListItem button component="a" href={path} onClick={handleClick} className={nested ? classes.nested : null}>
+        <ListItemText primary={treeNode.item.title} />
+        {openToggle}
+      </ListItem>
       {children}
-    </li>
-  )
+    </>
+  );
 }
 
 function makeEntry(data: DocsData): JSX.Element {
