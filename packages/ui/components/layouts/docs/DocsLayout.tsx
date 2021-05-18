6341f2c682d2850c5b0a2bc3babcbf147f7c0177
David S <github@davidsharer.com>
2021-05-17 21:59:11 -0700

    towards mobile layout

--- packages/ui/components/layouts/docs/DocsLayout.tsx
+++ packages/ui/components/layouts/docs/DocsLayout.tsx
@@ -2,6 +2,7 @@ import { DocsDataTree } from "../../../lib/markdown/docs/get-docs-data";
 import DocTree from "./DocTree";
 import NavTop from "../../navtop/NavTop";
 import styles from "./DocsLayout.module.scss";
+import NavSide from "../../navtop/NavSide";
 
 export default function DocsLayout({
   children,
@@ -11,18 +12,17 @@ export default function DocsLayout({
   allDocsData: DocsDataTree[];
 }) {
   return (
-    <div className={styles.root}>
-      <div className={styles.top}>
-        <NavTop></NavTop>
-      </div>
-      <div className={styles.bottom}>
-        <div className={styles.left}>
-          <DocTree allDocsData={allDocsData}></DocTree>
+    <div className={styles.trueRoot}>
+      <NavSide></NavSide>
+      <div className={styles.root}>
+        <div className={styles.top}>
+          <NavTop></NavTop>
         </div>
-        <div className={styles.right}>
-          <div className={styles.textContainer}>
-            {children}
+        <div className={styles.bottom}>
+          <div className={styles.left}>
+          <DocTree allDocsData={allDocsData}></DocTree>
           </div>
+          <div className={styles.right}>{children}</div>
         </div>
       </div>
     </div>
