fae5b1b9498481fda63ae44df451650b1ce0cdc4
David S <github@davidsharer.com>
2021-06-26 20:36:47 -0700

    Updating ingress to support rollem.rocks; Attempt to fix login

--- packages/ui/lib/configured-discord-oauth.ts
+++ packages/ui/lib/configured-discord-oauth.ts
@@ -3,7 +3,6 @@ import DiscordOauth2 from "discord-oauth2";
 const config = {
   clientId: process.env.DISCORD_CLIENT_ID,
   clientSecret: process.env.DISCORD_CLIENT_SECRET,
-  redirectUri: process.env.DISCORD_REDIRECT_URI,
   db_connection: process.env.DB_CONNECTION_STRING,
 };
 
