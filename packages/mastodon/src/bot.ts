52a4cf7b681138c8ce0ca4d0587cdf5288ed617a
David S <github@davidsharer.com>
2022-11-23 21:59:58 -0800

    Basic bot works

--- packages/mastodon/src/bot.ts
+++ packages/mastodon/src/bot.ts
@@ -1,4 +1,6 @@
-import { login } from 'masto';
+import { login, MastoClient, Notification, Status, Account } from 'masto';
+import { App } from "./app";
+import { ContainerV1, RollemParserV1, RollemParserV1Beta, RollemParserV2 } from "@rollem/language";
 
 function getSettings() {
   if (!process.env.MASTODON_URI) {
@@ -17,25 +19,166 @@ function getSettings() {
   };
 }
 
-async function main() {
-  const settings = getSettings();
+class RollemMastodon {
+  private masto: MastoClient;
 
-  console.log("pre-login", settings);
-  const masto = await login({
-    url: settings.uri,
-    accessToken: settings.token,
-  });
+  private me?: Account = undefined;
+  private followersLookup: Map<string, Account> = new Map<string, Account>();
+  private lastFollowerLookupTime?: Date = undefined;
+  private parser: RollemParserV1;
 
-  console.log("pre-toot");
+  constructor(masto: MastoClient) {
+    this.masto = masto;
+    this.parser = new RollemParserV1();
+  }
 
-  await masto.statuses.create({
-    status: 'Hello Mastodon!',
-    visibility: 'direct',
-  });
+  static async init() {
+    const settings = getSettings();
+  
+    console.log("pre-login", settings);
+    const masto = await login({
+      url: settings.uri,
+      accessToken: settings.token,
+    });
+    await new RollemMastodon(masto).subscribe();
+  }
 
-  console.log("post-toot");
+  private async updateFollowerCache() {
+    const me = await this.masto.accounts.verifyCredentials();
+    this.me = me;
+
+    const result = this.masto.accounts.getFollowersIterable(me.id, {});
+    const followers: Account[] = [];
+    let page = await result.next();
+    followers.push(...page.value as Account[]);
+    while (!page.done) {
+      page = await result.next();
+      if (page.value) {
+        followers.push(...page.value as Account[]);
+      }
+    }
+
+    const newLookup = new Map<string, Account>();
+    for (const follower of followers) {
+      newLookup.set(follower.acct, follower);
+    }
+
+    this.followersLookup = newLookup;
+    this.lastFollowerLookupTime = new Date(Date.now());
+    
+    console.log(`${newLookup.size} followers (${this.lastFollowerLookupTime.toISOString()})`);
+  }
+
+  private async subscribe() {
+    await this.updateFollowerCache();
+    const updateCacheDelayInMs = 60 * 1_000;
+    setInterval(() => this.updateFollowerCache(), updateCacheDelayInMs);
+
+    const timeline = await this.masto.stream.streamUser();
+
+    // Add handlers
+    timeline.on('update', this.handleUpdate);
+    timeline.on('notification', this.handleNotification);
+  }
+
+  private handleUpdate = async (status: Status) => {
+    if (status.account.id === this.me?.id) {
+      return;
+    }
+
+    if (status.account.bot) {
+      return;
+    }
+
+    const followsUs = !!this.followersLookup.get(status.account.acct);
+    const mentionedUs = status.mentions.some(m => m.id === this.me?.id);
+    const followTag = mentionedUs
+      ? '@mention'
+      : followsUs
+      ? 'follower'
+      : '-REMOVE-';
+    if (!followsUs && mentionedUs) {
+      this.masto.accounts.unfollow(status.account.id);
+      console.log(`Unfollowed ${status.account.acct}. No longer follows us.`);
+    }
+
+    if (!followsUs && !mentionedUs) {
+      return;
+    }
+
+    console.log(`|${followTag}| ${status.account.username}: ${status.content}`);
+
+    // handle inline matches
+    let last: RegExpExecArray | null = null;
+    let matches: string[] = [];
+    let regex = /\[(.+?)\]/g;
+    while (last = regex.exec(status.content)) {
+      matches.push(last[1]);
+    }
+
+    const results = matches.map(m => this.parser.tryParse(m));
+    const responses = results.map(r => this.buildMessage(r, false));
+    var realResponses = responses.filter(r => !!r);
+    if (realResponses.length > 0) {
+      const responseBody = realResponses.join("\n");
+      await this.masto.statuses.create({
+        status: responseBody,
+        inReplyToId: status.id,
+        visibility: status.visibility
+      });
+    }
+  };
+
+  protected buildMessage(result: ContainerV1 | false, requireDice = true) {
+    if (result === false) { return false; }
+    if (result.error) { return result.error; }
+    if (result.depth <= 1) { return false; }
+    if (requireDice && result.dice < 1) { return false; }
+
+    let response = "";
+
+    if (result.label && result.label !== "") {
+      response += "'" + result.label + "', ";
+    }
+
+    if (typeof (result.value) === "boolean") {
+      // special case for boolean results
+      result.value = result.value ? "**Success!**" : "**Failure!**";
+      response += result.value + ' ⟵ ' + result.pretties.split(']').join('] ');
+    } else {
+      //spacing out along with a nice formatting of the role number, for all other results
+      response += '` ' + result.value + ' `' + ' ⟵ ' + result.pretties.split(']').join('] ');
+    }
+
+    return response;
+  }
+
+  private handleNotification = async (notification: Notification) => {
+    // When your status got favourited, log
+    if (notification.type === 'favourite') {
+      console.log(`${notification.account.username} favourited your status!`);
+    }
+
+    // // When you got a mention, reply
+    // if (notification.type === 'mention' && notification.status) {
+    //   await this.masto.statuses.create({
+    //     status: 'I received your mention!',
+    //     inReplyToId: notification.status.id,
+    //     visibility: notification.status.visibility
+    //   });
+    // }
+
+    // When you got followed, follow them back
+    if (notification.type === 'follow') {
+      console.log("Following back", notification.account.acct);
+      await this.masto.accounts.follow(notification.account.id);
+    }
+  };
 }
 
-main().catch((error) => {
-  console.error(error);
+// main
+RollemMastodon.init().catch((error) => {
+  throw error;
 });
+
+const app = new App();
