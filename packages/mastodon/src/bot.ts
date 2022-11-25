8cca0772de73857f26bbf24a56ed8799bedecbd1
David S <github@davidsharer.com>
2022-11-24 16:50:24 -0800

    dodgy styling

--- packages/mastodon/src/bot.ts
+++ packages/mastodon/src/bot.ts
@@ -3,6 +3,25 @@ import { App } from "./app";
 import { default as LRUCache } from "lru-cache";
 import { ContainerV1, RollemParserV1, RollemParserV1Beta, RollemParserV2 } from "@rollem/language";
 
+import { transform, getFonts, revertTransform } from 'convert-unicode-fonts';
+const fonts = getFonts();
+
+enum CombiningSymbols {
+  ShortStrikethru = '\u0335',
+  LongStrikethru = '\u0336',
+  ShortSlash = '\u0337',
+  LongSlash = '\u0338',
+}
+
+function zipWithSymbol(text: string, symbol: CombiningSymbols) {
+  return symbol + text.split('').join(symbol);
+}
+
+interface CWResponse {
+  header: string;
+  belowFold: string;
+}
+
 function getSettings() {
   if (!process.env.MASTODON_URI) {
     throw new Error("Missing Mastodon URI");
@@ -133,23 +152,57 @@ class RollemMastodon {
 
     const results = matches.map(m => this.parser.tryParse(m));
     const responses = results.map(r => this.buildMessage(r, false));
-    var realResponses = responses.filter(r => !!r);
+    const realResponses = responses.filter(r => !!r) as CWResponse[];
     if (realResponses.length > 0) {
       console.log(realResponses);
-      let responseBody = realResponses.join("\n");
-      responseBody = `𝗨𝗻𝗶𝗰𝗼𝗱𝗲?\n` + responseBody;
+      let headerBody = realResponses.map(r => r.header).join(", ");
+      let belowFold = realResponses.map(r => r.belowFold).join("\n")
+
+      // apply bold-strikethru
+      belowFold = belowFold.replace(/\*\*\~\~(.+?)\~\~\*\*/g, s => {
+        const snipped = s.substring(4, s.length - 4);
+        const zipped = zipWithSymbol(snipped, CombiningSymbols.ShortStrikethru);
+        const transformed = transform(zipped, fonts['sansSerifBold']);
+        return transformed;
+      });
+      belowFold = belowFold.replace(/\~\~\*\*(.+?)\*\*\~\~/g, s => {
+        const snipped = s.substring(4, s.length - 4);
+        return zipWithSymbol(snipped, CombiningSymbols.LongStrikethru);
+      });
+
+      // apply bold
+      belowFold = belowFold.replace(/\*\*(.+?)\*\*/g, s => {
+        const snipped = s.substring(2, s.length - 2);
+        const transformed = transform(snipped, fonts['sansSerifBold']);
+        console.log(s, snipped, transformed);
+        return transformed;
+      });
+
+      // apply strikethru
+      belowFold = belowFold.replace(/\~\~(.+?)\~\~/g, s => {
+        const snipped = s.substring(2, s.length - 2);
+        return zipWithSymbol(snipped, CombiningSymbols.LongStrikethru);
+      });
+
+      // remove code ticks
+      belowFold = belowFold.replace(/`/g, '');
+
+      // responseBody = `𝗨𝗻𝗶𝗰𝗼𝗱𝗲?\n` + responseBody;
       const config: CreateStatusParams = {
-        status: responseBody,
+        spoilerText: headerBody,
+        status: belowFold,
         inReplyToId: status.id,
         visibility: 'unlisted',
       };
+
+      console.log(config);
       await this.masto.statuses.create(config);
     }
   };
 
-  protected buildMessage(result: ContainerV1 | false, requireDice = true) {
+  protected buildMessage(result: ContainerV1 | false, requireDice = true): CWResponse | false {
     if (result === false) { return false; }
-    if (result.error) { return result.error; }
+    if (result.error) { return { header: "error", belowFold: result.error }; }
     if (result.depth <= 1) { return false; }
     if (requireDice && result.dice < 1) { return false; }
 
@@ -168,7 +221,10 @@ class RollemMastodon {
       response += '` ' + result.value + ' `' + ' ⟵ ' + result.pretties.split(']').join('] ');
     }
 
-    return response;
+    return {
+      header: `${result.value} ${result.label}`,
+      belowFold: response,
+    };
   }
 
   private handleNotification = async (notification: Notification) => {
