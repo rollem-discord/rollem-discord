92fa6dec2bfc71f2f057de023389e6a215439f73
Hyperaxie <jwoods971@gmail.com>
2023-10-07 02:18:52 -0700

    Create index.tsx
    Weird Nonsense going on with some failure to build

---
+++ packages/ui/pages/owlbear/index.tsx
@@ -0,0 +1,20 @@
+import DocsLayout from "../components/layouts/docs/DocsLayout"
+import { DocsDataTree, makePropsAllDocData } from "../lib/markdown/docs/get-docs-data"
+
+export default function Custom404({ allDocsData }: { allDocsData: DocsDataTree[] }) {
+  return (
+    <DocsLayout allDocsData={allDocsData}>
+      <h1>404 - Page Not Found</h1>
+      <img src="/images/rollem-transparent.png"></img>
+      <p>You may have been looking for one of the docs to the left.</p>
+    </DocsLayout>
+  )
+}
+
+export async function getStaticProps(): Promise<{ props: { allDocsData: DocsDataTree[]} }> {
+  return {
+    props: {
+      ...makePropsAllDocData(),
+    }
+  }
+}
